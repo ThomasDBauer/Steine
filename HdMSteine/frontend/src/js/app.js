8ed31496a5a2f19f2931a2e41291a011a9de9502
ypislon <ypislon@freenet.de>
2016-04-23 06:10:17 +0200

    merge conflict removed

--- HdMSteine/frontend/src/js/app.js
+++ HdMSteine/frontend/src/js/app.js
@@ -1,34 +1,24 @@
-<<<<<<< HEAD
-var waehrung = angular.module('waehrung', ['ui.router', 'waehrung.controllers', 'waehrung.services']);
-
-waehrung
-.run();
-=======
 angular.module('steine', ['ng', 'ngResource', 'ui.router', 'steine.controllers'])
 
-.run(function () {
-    console.log('hi world')
-})
-
->>>>>>> manu
-
-waehrung
+    .run(function () {
+        console.log('hi world')
+    })
     .config(function ($stateProvider, $urlRouterProvider) {
-    // add config here
+        // add config here
 
-    $stateProvider
-        .state('app', {
-            url: '/',
-            templateUrl: '../../src/www/index.html',
-            controller: 'AppCtrl'
-        })
+        $stateProvider
+            .state('app', {
+                url: '/',
+                templateUrl: '../../src/www/index.html',
+                controller: 'AppCtrl'
+            })
 
-        .state('app.login', {
-            url: '/login',
-            templateUrl: '../../src/www/login.html',
-            controller: 'LoginCtrl'
-    });
+            .state('app.login', {
+                url: '/login',
+                templateUrl: '../../src/www/login.html',
+                controller: 'LoginCtrl'
+        });
 
-    // if none of the above states are matched, use this as the fallback
+        // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/login');
-});
+    });
--- HdMSteine/frontend/src/js/controllers.js
+++ HdMSteine/frontend/src/js/controllers.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-waehrung.angular.module('waehrung.controllers', [])
-=======
 angular.module('steine.controllers', [])
-
->>>>>>> manu
     .controller('AppCtrl', function ($scope) {
         $scope.hello = "Hello World from Angular!"
     })
