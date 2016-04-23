18e62e3d48c4264ccff8d14847a27af7cad78733
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-04-23 05:47:49 +0200

    seeder works

---
+++ HdMSteine/backend/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 'name', 'email', 'password'
+
+        DB::table('users')->delete();
+        User::create(['name'=>'Thomas', 'email'=>'t', 'password'=>'123456']);
+        User::create(['name'=>'Manu', 'email'=>'m', 'password'=>'123456']);
+        User::create(['name'=>'Hanna', 'email'=>'h', 'password'=>'123456']);
+
+    }
+}
